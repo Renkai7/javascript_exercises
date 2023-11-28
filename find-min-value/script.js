bbdd35eaea3d1d2a24d772393705a49d592ec109
Michael Spencer <ms53150@gmail.com>
2023-11-28 18:50:26 -0500

    create linear search for an array

---
+++ find-min-value/script.js
@@ -0,0 +1,16 @@
+// This loops through an array to find the lowest value
+const findMinValue = (arr) => {
+	let minValue = arr[0];
+
+	for (let i = 0; i < arr.length; i++) {
+		if (minValue > arr[i]) {
+			minValue = arr[i];
+		}
+	}
+
+	return minValue;
+};
+
+const numbers = [5, 3, 9, 1, 7, 2];
+const min = findMinValue(numbers);
+console.log(`The minimum value in the array is: ${min}.`);
