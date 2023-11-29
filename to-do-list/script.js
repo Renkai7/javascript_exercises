c60e9814a7eddab429b862dec7c4cfe38b2a0cce
Michael Spencer <ms53150@gmail.com>
2023-11-29 17:29:32 -0500

    add function that adds task label and checkbox

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,3 +1,20 @@
 // DOM UIs
 const taskInput = document.querySelector("#task-input");
 const addButton = document.querySelector("#add-button");
+const taskList = document.querySelector("#task-list");
+
+const addTask = () => {
+	const newCheckbox = document.createElement("input");
+	const newTask = document.createElement("label");
+	const newListItem = document.createElement("li");
+
+	newCheckbox.setAttribute("type", "checkbox");
+
+	newCheckbox.addEventListener("change", crossOut);
+	newTask.innerHTML = taskInput.value;
+	taskList.appendChild(newListItem);
+	newListItem.appendChild(newCheckbox);
+	newListItem.appendChild(newTask);
+};
+
+addButton.addEventListener("click", addTask);
