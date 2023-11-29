f4807019c4f60f53c56e3e7362ceba8abd6ed6b7
Michael Spencer <ms53150@gmail.com>
2023-11-29 17:30:16 -0500

    add function to cross out task when checked off

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -3,6 +3,13 @@ const taskInput = document.querySelector("#task-input");
 const addButton = document.querySelector("#add-button");
 const taskList = document.querySelector("#task-list");
 
+const crossOut = (event) => {
+	const checkbox = event.target;
+
+	const label = checkbox.nextElementSibling;
+	label.classList.toggle("line-through");
+};
+
 const addTask = () => {
 	const newCheckbox = document.createElement("input");
 	const newTask = document.createElement("label");
