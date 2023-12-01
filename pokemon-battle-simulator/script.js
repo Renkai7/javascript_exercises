d883324d26cf1c8912e742dad0b6e5e2d22eaa5e
Michael Spencer <ms53150@gmail.com>
2023-11-30 21:26:28 -0500

    create battleTurn function to decrease enemy health based on attack power

--- pokemon-battle-simulator/script.js
+++ pokemon-battle-simulator/script.js
@@ -1,6 +1,16 @@
-const Pokemon = (name, type, hp, attack) => {
+// Constructor function for Pokemon objects
+function Pokemon(name, type, hp, attack) {
 	this.name = name;
 	this.type = type;
 	this.hp = hp;
 	this.attack = attack;
+}
+
+// Create Pokemon
+const pikachu = new Pokemon("Pikachu", "Electric", 150, 90);
+const charmander = new Pokemon("Charmander", "Fire", 130, 110);
+
+const battleTurn = (playerPokemon, enemyPokemon) => {
+	const damage = playerPokemon.attack;
+	enemyPokemon.hp = enemyPokemon.hp - damage;
 };
