042580dc16e1b7727a52c033b7c10315b034b3b4
Michael Spencer <ms53150@gmail.com>
2023-12-04 20:07:46 -0500

    add doubled array using array mapping

---
+++ mapping-array-basics/script.js
@@ -0,0 +1,11 @@
+const numArray = [5, 2, 8, 5, 9, 3, 6, 2, 1, 4];
+
+// Double the numbers of Array elements
+const doubleArray = numArray.map((num) => {
+	return num * 2;
+});
+
+console.log("Double numbers: ", doubleArray);
+
+// Square the numbers of Array elements
+const 
