736e0028a9a344f0a13ca43a49caa682b9f6ea69
Michael Spencer <ms53150@gmail.com>
2023-12-04 20:28:16 -0500

    add map to increase each number by 10

--- mapping-array-basics/script.js
+++ mapping-array-basics/script.js
@@ -1,5 +1,8 @@
 const numArray = [5, 2, 8, 5, 9, 3, 6, 2, 1, 4];
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
 
+console.log("Original letter array: ", letters);
+console.log("Original number array: ", numArray);
 // Double the numbers of Array elements
 const doubleArray = numArray.map((num) => {
 	return num * 2;
@@ -13,3 +16,12 @@ const squareArray = numArray.map((num) => {
 });
 
 console.log("Square numbers: ", squareArray);
+
+// Add 10 to each element
+const add10ToArray = numArray.map((num) => {
+	return num + 10;
+});
+
+console.log("Add 10 to numbers: ", add10ToArray);
+
+// Convert to
