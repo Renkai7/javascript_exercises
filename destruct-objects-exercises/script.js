7dc0a335927e9678db8808772451400eeb3a9ed1
Michael Spencer <ms53150@gmail.com>
2023-12-05 21:03:04 -0500

    add example for destructuring with different variable names

--- destruct-objects-exercises/script.js
+++ destruct-objects-exercises/script.js
@@ -7,3 +7,13 @@ const person = {
 
 const { name, age } = person;
 console.log(name, age);
+
+// Destructuring with Different Variable Names
+const book = {
+	title: "JavaScript Essentials",
+	author: "Jane Doe",
+	year: 2021,
+};
+
+const { title: bookTitle, author: bookAuthor } = book;
+console.log(bookTitle, bookAuthor);
