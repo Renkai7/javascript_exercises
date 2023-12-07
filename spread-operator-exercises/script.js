7acb45153fde8f0ffe4b6a95952653f248c6ebe6
Michael Spencer <ms53150@gmail.com>
2023-12-06 19:11:06 -0500

    copy array using spread operator

--- spread-operator-exercises/script.js
+++ spread-operator-exercises/script.js
@@ -4,4 +4,7 @@ let array2 = [4, 5, 6];
 const array3 = [...array1, ...array2];
 console.log(array3);
 
-//
+// Copying an Array
+let originalArray = ["a", "b", "c"];
+const copiedArray = [...originalArray];
+console.log(copiedArray);
