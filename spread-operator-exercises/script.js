c8fcdf71b72895df605b6b026ed4968a0d9a90b0
Michael Spencer <ms53150@gmail.com>
2023-12-06 19:42:13 -0500

    copy an object with spread operator

--- spread-operator-exercises/script.js
+++ spread-operator-exercises/script.js
@@ -42,3 +42,8 @@ let obj1 = { a: 1, b: 2 };
 let obj2 = { c: 3, d: 4 };
 const obj3 = { ...obj1, ...obj2 };
 console.log(obj3);
+
+// Copying an Object
+let person = { name: "John", age: 30 };
+const copyPerson = { ...person };
+console.log(copyPerson);
