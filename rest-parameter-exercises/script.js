4bb53d81a82eae3015e2c3764e1600ecd29a750a
Michael Spencer <ms53150@gmail.com>
2023-12-07 20:29:07 -0500

    create sum function using rest parameters

---
+++ rest-parameter-exercises/script.js
@@ -0,0 +1,10 @@
+// Basic Rest Parameters
+const sum = (...numbers) => {
+	let total = 0;
+	for (let i = 0; i < numbers.length; i++) {
+		total += numbers[i];
+	}
+	return total;
+};
+
+console.log(sum(1, 2, 3));
