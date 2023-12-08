52d1ee1e48bb26c93bbc011c786ea3a9507dcc37
Michael Spencer <ms53150@gmail.com>
2023-12-07 21:33:09 -0500

    complete complex destructure

--- rest-parameter-exercises/script.js
+++ rest-parameter-exercises/script.js
@@ -56,4 +56,10 @@ console.log(combineArray(1, 2, 3, 4));
 // Default Values with Destructuring
 const scores = [10, 20];
 const [numOne = scores[0], numTwo = scores[1], numThree = 3] = scores;
-console.log(scores);
+
+// Complex Destructuring
+const student = { name: "Emily", age: 22, scores: { maths: 74, english: 63 } };
+const getStudentNameAndGrade = ({ name, scores }) => {
+	return `${name} ${scores.maths}`;
+};
+console.log(getStudentNameAndGrade(student));
