dbe98ebc06e89571a4d3704a9e1c31f4f33ed312
Michael Spencer <ms53150@gmail.com>
2023-12-09 09:28:48 -0500

    complete task 6

--- data-structure-operations-exercises/script.js
+++ data-structure-operations-exercises/script.js
@@ -78,3 +78,11 @@ console.log(players1Final);
 // 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
 const { team1, x: draw, team2 } = game.odds;
 console.log(team1, draw, team2);
+
+// 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+
+const printGoals = (...players) => {
+	const playerLength = players.length;
+	return `${playerLength} goals were scored`;
+};
+console.log(printGoals(...game.scored));
