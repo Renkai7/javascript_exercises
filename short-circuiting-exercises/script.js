cd119111fff95da98dd8160f6fb19c3f53d623d8
Michael Spencer <ms53150@gmail.com>
2023-12-10 13:00:15 -0500

    add short-circuiting || operator

--- short-circuiting-exercises/script.js
+++ short-circuiting-exercises/script.js
@@ -7,3 +7,14 @@ let isLoggedIn = true;
 let hasPrivileges = true;
 
 isLoggedIn && hasPrivileges && showAdminPanel();
+
+// OR Short-Circuit (||)
+const getDefaultUsername = () => {
+	username = "guest";
+	return username;
+};
+
+let username = "";
+
+username || getDefaultUsername();
+console.log(username);
