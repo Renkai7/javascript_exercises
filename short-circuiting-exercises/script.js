4a73f6dbfbfc609d8ad6783f8fd5c1928763daa8
Michael Spencer <ms53150@gmail.com>
2023-12-10 13:50:33 -0500

    combine 'AND' and 'OR' short-circuiting

--- short-circuiting-exercises/script.js
+++ short-circuiting-exercises/script.js
@@ -18,3 +18,14 @@ let username = "";
 
 username || getDefaultUsername();
 console.log(username);
+
+// Combining AND and OR
+let isAuthenticated = true;
+let hasError = false;
+let isAdmin = false;
+
+if ((isAuthenticated && !hasError) || isAdmin) {
+	console.log("Access granted!");
+} else {
+	console.log("Access...DENIED!");
+}
