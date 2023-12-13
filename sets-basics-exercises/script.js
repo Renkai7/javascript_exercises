f0d97d85beb2dacd953313483ed07a5708af4630
Michael Spencer <ms53150@gmail.com>
2023-12-13 14:48:36 -0500

    create function to combine two sets

--- sets-basics-exercises/script.js
+++ sets-basics-exercises/script.js
@@ -9,3 +9,15 @@ console.log(numSet);
 // Union of Sets
 const setA = new Set([1, 2, 3]);
 const setB = new Set([4, 3, 2]);
+
+const combineSets = (setOne, setTwo) => {
+	const mergedSet = new Set();
+	for (const item of setOne) {
+		mergedSet.add(item);
+	}
+	for (const item of setTwo) {
+		mergedSet.add(item);
+	}
+	return mergedSet;
+};
+console.log(combineSets(setA, setB));
