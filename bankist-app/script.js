c9129a340a577ebed9feaaf83c65a9d590ac38e2
Michael Spencer <ms53150@gmail.com>
2023-12-31 16:38:04 -0500

    add sort, account close, and loan functionality

--- bankist-app/script.js
+++ bankist-app/script.js
@@ -61,10 +61,12 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-const displayMovements = function (movements) {
+const displayMovements = (movements, sort = false) => {
 	containerMovements.innerHTML = "";
 
-	movements.forEach(function (mov, i) {
+	const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+	movs.forEach(function (mov, i) {
 		const type = mov > 0 ? "deposit" : "withdrawal";
 
 		const html = `
@@ -194,6 +196,52 @@ btnTransfer.addEventListener("click", (e) => {
 		// Update UI
 		updateUI(currentAccount);
 	}
+});
+
+btnLoan.addEventListener("click", (e) => {
+	e.preventDefault();
+
+	const amount = Number(inputLoanAmount.value);
+
+	if (
+		(amount > 0) &
+		currentAccount.movements.some((mov) => mov >= amount * 0.1)
+	) {
+		// Add movement
+		currentAccount.movements.push(amount);
+
+		// Update UI
+		updateUI(currentAccount);
+	}
+	inputLoanAmount.value = "";
+});
+
+btnClose.addEventListener("click", (e) => {
+	e.preventDefault();
+
+	if (
+		inputCloseUsername.value === currentAccount.username &&
+		Number(inputClosePin.value) === currentAccount.pin
+	) {
+		const index = accounts.findIndex(
+			(acc) => acc.username === currentAccount.username
+		);
+
+		// Delete account
+		accounts.splice(index, 1);
+
+		// Hide UI
+		containerApp.style.opacity = 0;
+	}
+
+	inputCloseUsername.value = inputClosePin.value = "";
+});
+
+let sorted = false;
+btnSort.addEventListener("click", (e) => {
+	e.preventDefault();
+	displayMovements(currentAccount.movements, !sorted);
+	sorted = !sorted;
 });
 
 /////////////////////////////////////////////////
