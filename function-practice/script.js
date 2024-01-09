04dce04ee386698e832d0ee6f507a3d10925de89
Michael Spencer <ms53150@gmail.com>
2024-01-09 13:17:25 -0500

    create function to return persona's level

--- function-practice/script.js
+++ function-practice/script.js
@@ -6,3 +6,8 @@ console.log(defaultPersona());
 console.log(defaultPersona("Johanna"));
 
 // Persona Strength
+const personaStrength = function (name, level = 1) {
+	return `Persona: ${name} | Level: ${level}`;
+};
+console.log(personaStrength("Arsène"));
+console.log(personaStrength("Arsène", 80));
