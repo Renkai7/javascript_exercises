7e65d4699e78f420f6df51e14b26873a475e0698
Michael Spencer <ms53150@gmail.com>
2024-01-09 13:24:10 -0500

    create a function to calculate HP based on level and function that fuses personas

--- function-practice/script.js
+++ function-practice/script.js
@@ -11,3 +11,19 @@ const personaStrength = function (name, level = 1) {
 };
 console.log(personaStrength("Arsène"));
 console.log(personaStrength("Arsène", 80));
+
+// Calculate HP
+const calculateHP = function (level, baseHP = 50) {
+	const personaHP = level * baseHP;
+
+	return `A level ${level} Persona has ${personaHP} HP`;
+};
+console.log(calculateHP(2));
+
+// Persona Fusion
+const personaFusion = function (persona1, persona2) {
+	const fusedPersona =
+		persona1.slice(0, 3) + persona2.slice(0, 3).toLowerCase();
+	return `${persona1} and ${persona2} fuse together to become: ${fusedPersona}`;
+};
+console.log(personaFusion("Arsène", "Johanna"));
