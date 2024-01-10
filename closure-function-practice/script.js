8fb53252d172c386278c6aaac17bbff295868a75
Michael Spencer <ms53150@gmail.com>
2024-01-09 20:22:01 -0500

    add closure function for ninja academy enrollment

--- closure-function-practice/script.js
+++ closure-function-practice/script.js
@@ -9,3 +9,46 @@ const createShadowClone = function (cloneCount) {
 const shadowCloneJutsu = createShadowClone(1);
 shadowCloneJutsu();
 shadowCloneJutsu();
+
+// Chakra Control
+const chakraControl = function () {
+	let chakraLevel = 100;
+
+	return function (n) {
+		chakraLevel -= n;
+		console.log(`Chakra level at ${chakraLevel}`);
+	};
+};
+
+const useJutsu = chakraControl();
+useJutsu(10);
+useJutsu(10);
+
+// Rasengan Training
+const rasenganTraining = function () {
+	let practiceCount = 0;
+
+	return function () {
+		practiceCount++;
+		console.log(practiceCount);
+	};
+};
+
+const practiceJutsu = rasenganTraining();
+practiceJutsu();
+practiceJutsu();
+
+// Ninja Academy Enrollment
+const ninjaAcademy = function () {
+	const studentsEnrolled = [];
+
+	return function (student) {
+		studentsEnrolled.push(student);
+		console.log(...studentsEnrolled);
+	};
+};
+
+const enrollStudent = ninjaAcademy();
+enrollStudent("Naruto");
+enrollStudent("Sasuke");
+enrollStudent("Sakura");
