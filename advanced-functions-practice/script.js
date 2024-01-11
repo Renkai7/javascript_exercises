8f43f204a5544b24e90c48bc30d481bd70f4f5dd
Michael Spencer <ms53150@gmail.com>
2024-01-10 20:06:34 -0500

    change folder name

---
+++ advanced-functions-practice/script.js
@@ -0,0 +1,148 @@
+// Display Character Names
+const characters = ["Haseo", "Alkaid", "Atoli", "Pi"];
+const displayCharacters = function (arr) {
+	return arr;
+};
+const characterIntro = function (arr, fn) {
+	const characterList = fn(arr);
+	characterList.map((name) => {
+		console.log(name);
+	});
+	return characterList;
+};
+console.log(characterIntro(characters, displayCharacters));
+
+// Calculate Levels
+const displayLevel = function () {
+	let level = 10;
+	return level;
+};
+const expToLevelUp = function (name, fn) {
+	const calcExp = fn() * 1000;
+	return `${name} needs ${calcExp} to level up.`;
+};
+console.log(expToLevelUp(characters[0], displayLevel));
+
+// List Guild Members
+const getGuildMembers = function (arr) {
+	return [...arr];
+};
+const getGuildRanks = function (arr, fn) {
+	const members = fn(arr);
+	members.map((member) => {
+		console.log(`${member}'s guild rank: 'Rookie'`);
+	});
+};
+getGuildRanks(characters, getGuildMembers);
+
+// Item Availability Checker
+const itemInventory = function () {
+	const inventory = ["healing potion", "smokescreen", "gold axe"];
+	return [...inventory];
+};
+
+const checkItemAvaility = function (item, fn) {
+	const playerInventory = fn();
+
+	if (!playerInventory.includes(item)) {
+		return `${item} is not in your inventory.`;
+	}
+
+	return `You have ${item} in your inventory`;
+};
+console.log(checkItemAvaility("potion", itemInventory));
+console.log(checkItemAvaility("gold axe", itemInventory));
+
+// Map Locations Revealer
+const mapDetails = function () {
+	const maps = [
+		{
+			location: "Mac Anu",
+			secret: "the R:2 version of town is bigger than the R:1 version.",
+		},
+		{
+			location: "Lumina Cloth",
+			secret: "it is a PVP zone",
+		},
+	];
+
+	return [...maps];
+};
+
+const mapRevealer = function (location, fn) {
+	const maps = fn();
+	for (const obj of maps) {
+		if (location === obj.location) {
+			return `Here is a secret about ${obj.location}: ${obj.secret}`;
+		}
+	}
+	return "Location doesn't exist";
+};
+// mapRevealer("Mac Anu", mapDetails);
+console.log(mapRevealer("Mac Anu", mapDetails));
+console.log(mapRevealer("Tac Anu", mapDetails));
+
+// Character Bio Updater
+const updateBio = function (nameChange) {
+	const name = nameChange;
+	const characterBio = "Adept Rogue";
+	const characterInfo = {
+		name: name,
+		bio: characterBio,
+	};
+
+	return characterInfo;
+};
+
+const characterBioUpdater = function (fn, updatedName = "Haseo") {
+	const nameChange = fn(updatedName);
+	return `${nameChange.name}: ${nameChange.bio}`;
+};
+console.log(characterBioUpdater(updateBio, "Alkaid"));
+console.log(characterBioUpdater(updateBio));
+
+// Guild Event Scheduler
+const guildEvent = function (event, time) {
+	return `Guild Event: ${event} at ${time}`;
+};
+
+const scheduleGuildEvent = function (fn, event, time) {
+	const newEvent = fn(event, time);
+	return newEvent;
+};
+console.log(scheduleGuildEvent(guildEvent, "Raid", 9));
+
+// Battle Simulator
+const battleOutcome = function (player, enemy) {
+	return `${player} defeats ${enemy}`;
+};
+
+const setUpBattle = function (player, enemy, fn) {
+	const decideWinner = fn(player, enemy);
+	return decideWinner;
+};
+console.log(setUpBattle("Haseo", "Golem", battleOutcome));
+
+// Item Crafting System
+const craftItem = function (item1, item2) {
+	const craftOutcome =
+		item1 === "iron" && item2 === "cloth" ? "iron armor" : "Failed to craft";
+	return craftOutcome;
+};
+
+const startCrafting = function (item1, item2, fn) {
+	const craftedItem = fn(item1, item2);
+	return craftedItem;
+};
+console.log(startCrafting("iron", "cloth", craftItem));
+
+// Skill Learning
+const learnNewSkill = function (skill) {
+	return skill;
+};
+
+const useSkillBook = function (skill, fn) {
+	const learnedSkill = fn(skill);
+	return `You learned ${learnedSkill}!`;
+};
+console.log(useSkillBook("fireball", learnNewSkill));
