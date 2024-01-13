1eb20d648c20326f664b57ef35a5fea78e5e7159
Michael Spencer <ms53150@gmail.com>
2024-01-12 22:05:38 -0500

    add function to clear completed list

--- array-method-practice/script.js
+++ array-method-practice/script.js
@@ -6,23 +6,17 @@ const addItemToList = function (item) {
 	todos.push(item);
 };
 
-addItemToList("Buy milk");
-addItemToList("Cook breakfast");
-console.log("to-do list: ", [...todos]);
-
 // Exercise 2: Displaying Todos
 const displayTodoItems = function (todoList) {
 	todoList.forEach((todo) => {
 		console.log(todo);
 	});
 };
-displayTodoItems(todos);
 
 // Exercise 3: Counting Todos
 const showNumberOfTodos = function (todoList) {
 	return todoList.length;
 };
-console.log(showNumberOfTodos(todos));
 
 // Exercise 4: Finding a Specific Todo
 const indexOfTodoItem = function (todoList, itemToFind) {
@@ -32,14 +26,11 @@ const indexOfTodoItem = function (todoList, itemToFind) {
 	}
 	return indexOfItem;
 };
-console.log(indexOfTodoItem(todos, "Buy milk"));
 
 // Exercise 5: Deleting a Todo
 const removeTodoItem = function (todoList, itemToRemove) {
 	todoList.splice(itemToRemove, 1);
 };
-removeTodoItem(todos, "Buy milk");
-console.log("to-do list: ", [...todos]);
 
 // Exercise 6: Updating a Todo
 const updateToDoItem = function (
@@ -54,6 +45,65 @@ const updateToDoItem = function (
 	}
 	return;
 };
+
+// Exercise 7: Filtering Todos
+const filterListForWord = function (todoList, filterWord) {
+	filterWord = filterWord.toLowerCase();
+	const filteredList = todoList.filter((item) => {
+		item = item.toLowerCase();
+		return item.includes(filterWord);
+	});
+	return filteredList;
+};
+// Exercise 9: Finding Completed Todos
+const filterForCompletedTask = function (todoList) {
+	const filteredList = todoList.filter((item) => item.isChecked);
+	return filteredList;
+};
+// Exercise 8: Checking Off a Todo
+const checkOffTodoItem = function (todoList, checkedOffItem) {
+	const updatedList = todoList.map((item) => {
+		if (item === checkedOffItem) {
+			return { itemName: item, isChecked: true };
+		}
+		return { itemName: item };
+	});
+	updateList(todoList, updatedList);
+};
+
+// Exercise 10: Clearing Completed Todos
+const clearCompletedTasks = function (todoList) {
+	const filteredList = todoList.filter((item) => !item.isChecked);
+	updateList(todoList, filteredList);
+};
+
+const updateList = function (todoList, updatedList) {
+	todoList.length = 0;
+	todoList.push(...updatedList);
+};
+
 addItemToList("Buy milk");
+addItemToList("Cook breakfast");
+console.log("to-do list: ", [...todos]);
+
+displayTodoItems(todos);
+console.log(showNumberOfTodos(todos));
+
+console.log(indexOfTodoItem(todos, "Buy milk"));
+
+removeTodoItem(todos, "Buy milk");
+console.log("to-do list: ", [...todos]);
+
+addItemToList("Buy milk");
+addItemToList("Buy videogames");
 updateToDoItem(todos, "Buy milk", "Buy soda", indexOfTodoItem);
 console.log("to-do list: ", [...todos]);
+console.log("to-do list (filter: Buy): ", filterListForWord(todos, "Buy"));
+
+checkOffTodoItem(todos, "Buy soda");
+console.log("to-do list (checked off): ", [...todos]);
+
+console.log("Completed items: ", filterForCompletedTask(todos));
+
+clearCompletedTasks(todos);
+console.log("to-do list (after being cleared): ", [...todos]);
